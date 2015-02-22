75f6fb5890bce4b84cff211fa159e285676de9be
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2015-02-22 06:41:43 +0530

    Allow decryption if password is present

--- lib/Confy/Config.php
+++ lib/Confy/Config.php
@@ -2,29 +2,65 @@
 
 namespace Confy;
 
-use Confy\Client;
+use Confy\HttpClient\HttpClient;
 
 class Config
 {
     public static function load($url = array()) {
         if (gettype($url) == 'string') {
-            preg_match('/(https?:\/\/)(.*):(.*)@(.*)\/orgs\/([a-z0-9]*)\/projects\/([a-z0-9]*)\/envs\/([a-z0-9]*)\/config/i', $url, $matches);
+            $nameRegex = '([a-z0-9][a-z0-9-]*[a-z0-9])';
+            $pathRegex = 'orgs\\/'.$nameRegex.'\\/projects\\/'.$nameRegex.'\\/envs\\/'.$nameRegex;
+            $urlRegex = '/(https?:\\/\\/)(.*):(.*)@(.*)\\/('.$pathRegex.'|heroku)\\/config/i';
+
+            preg_match($urlRegex, $url, $matches);
 
             if (count($matches) == 0) {
-                throw new Exception('Invalid url');
+                throw new \Exception('Invalid url');
             }
 
             $url = array(
-                'host' => $matches[1].$matches[4], 'user' => $matches[2], 'pass' => $matches[3],
-                'org' => $matches[5], 'project' => $matches[6], 'env' => $matches[7]
+                'host' => $matches[1].$matches[4], 'path' => '/'.$matches[5].'/config',
+                'user' => $matches[2], 'pass' => $matches[3]
             );
         }
 
-        $client = new Client(array(
+        if (gettype($url) != 'array') {
+            throw new \Exception('Invalid url');
+        }
+
+        $httpClient = new HttpClient(array(
             'username' => $url['user'], 'password' => $url['pass']
         ), array('base' => $url['host']));
 
-        return $client->config($url['org'], $url['project'], $url['env'])->retrieve()->body;
+        $body = $httpClient->get($url['path'])->body;
+
+        if (gettype($body) == 'array') {
+            return $body;
+        }
+
+        $decryptPass = getenv('CONFY_DECRYPT_PASS');
+
+        if (gettype($body) != 'string') {
+            throw new \Exception('Invalid credential document');
+        }
+
+        if (empty($decryptPass)) {
+            throw new \Exception('No decryption password found. Fill env var CONFY_DECRYPT_PASS', 1);
+        }
+
+        $iv = base64_decode(substr($body, 0, 24));
+        $key = md5($decryptPass);
+
+        $decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, base64_decode(substr($body, 24)), MCRYPT_MODE_CBC, $iv);
+        $decrypted = substr($decrypted, 0, -ord($decrypted[strlen($decrypted) - 1]));
+
+        $body = json_decode($decrypted);
+
+        if (JSON_ERROR_NONE != json_last_error()) {
+            throw new \Exception('Decryption password is wrong');
+        }
+
+        return $body;
     }
 
     public static function env($url = array()) {
